8156032686c53ba782f6673a31123046556d8518
Jatin <jatin.parate@mlveda.com>
2022-05-10 23:39:07 +0530

    Updated generateEnvFile

--- generateEnvFile.js
+++ generateEnvFile.js
@@ -4,7 +4,7 @@ const dotEnv = require("dotenv");
 
 const dotEnvResult = dotEnv.config().parsed;
 
-console.log(dotEnvResult)
+console.log(dotEnvResult);
 
 const env = {
   EMAIL: process.env.EMAIL,
@@ -18,8 +18,8 @@ fs.writeFileSync(
 `
 );
 
-
 console.log(JSON.stringify(fs.readdirSync(path.resolve(), null, 2)));
 console.log(JSON.stringify(fs.readdirSync(path.resolve("src"), null, 2)));
 
 console.log(JSON.stringify(env, null, 2));
+console.log(fs.readFileSync(path.resolve("src", "environment_variables.ts")));
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
+import environment_variables from 'src/environment_variables';
+
 export const environment: { [key: string]: any } = {
+  ...environment_variables,
   production: true,
 };
